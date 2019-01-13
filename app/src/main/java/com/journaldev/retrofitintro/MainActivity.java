f3d37a9e726cf915ec7965b6ac69b97b35133a64
kaps100 <kapil.parmar@coveiot.com>
2019-01-14 00:23:18 +0530

    changed project to MVP pattern

--- app/src/main/java/com/journaldev/retrofitintro/MainActivity.java
+++ app/src/main/java/com/journaldev/retrofitintro/MainActivity.java
@@ -1,23 +1,19 @@
 package com.journaldev.retrofitintro;
 
-import android.provider.ContactsContract;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.JsonReader;
-import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import com.journaldev.retrofitintro.pojo.DataModel;
 import com.journaldev.retrofitintro.pojo.ResponseModel;
-import com.journaldev.retrofitintro.pojo.adaper.RecyclerAdapter;
+import com.journaldev.retrofitintro.adaper.RecyclerAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,89 +27,47 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import static java.security.AccessController.getContext;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoadData {
 
     TextView responseText;
-    APIInterface apiInterface;
     RecyclerView recyclerViewSettings;
     List<DataModel> resultList;
+    Context context;
+    ProgressBar progressBar;
+    PresenterLoadData presenterLoadData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        apiInterface = APIClient.getClient().create(APIInterface.class);
+        context = this;
          recyclerViewSettings = findViewById(R.id.recyclerview);
-
          resultList= new ArrayList<>();
-        /**
-         GET List Resources
-         **/
-        Call<ResponseModel> call = apiInterface.doGetListResources();
-        call.enqueue(new Callback<ResponseModel>() {
-            @Override
-            public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+         progressBar= findViewById(R.id.progressbar);
+        presenterLoadData = new PresenterLoadData(context,this);
+          presenterLoadData.showProgressBar(true);
+          presenterLoadData.LoadNeworkData();
 
 
-                String displayResponse = "";
-
-                ResponseModel responseModel = response.body();
-                String json = new Gson().toJson(response.body() );
-                try {
-                    JSONObject obj = new JSONObject(json);
-                    JSONArray jsonArray = obj.getJSONArray("rows");
-
-                    ArrayList<DataModel> playersList= (ArrayList<DataModel>) fromJson(jsonArray.toString(),
-                            new TypeToken<ArrayList<DataModel>>() {
-                            }.getType());
-
-
-
-                    RecyclerAdapter recyclerAdapter = new RecyclerAdapter(playersList,MainActivity.this);
-                    recyclerViewSettings.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-
-                    recyclerViewSettings.setAdapter(recyclerAdapter);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<ResponseModel> call, Throwable t) {
-
-            }
-        });
 
     }
-    public static  List<DataModel> getObjectList(String jsonString,Class<DataModel> cls){
-        List<DataModel> list = new ArrayList<DataModel>();
-        try {
-            Gson gson = new Gson();
-            JsonArray arry = new JsonParser().parse(jsonString).getAsJsonArray();
-            for (JsonElement jsonElement : arry) {
-                list.add(gson.fromJson(jsonElement, cls));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-    class NameList{
-        List<DataModel> list;
-        //getter and setter
 
-        public List<DataModel> getList() {
-            return list;
-        }
 
-        public void setList(List<DataModel> list) {
-            this.list = list;
-        }
+    @Override
+    public void loaddata(List<DataModel> loaddata) {
+        presenterLoadData.showProgressBar(false);
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(loaddata,MainActivity.this);
+        recyclerViewSettings.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        recyclerViewSettings.setAdapter(recyclerAdapter);
+
     }
-    public static Object fromJson(String jsonString, Type type) {
-        return new Gson().fromJson(jsonString, type);
+    @Override
+    public void DisplayProgressBar(boolean show) {
+
+        if (show)
+            progressBar.setVisibility(View.VISIBLE);
+        else
+            progressBar.setVisibility(View.GONE);
     }
-}
+
+   }
